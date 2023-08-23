e7f33bf7599bb708eedb11c5b835e17637121a3d
sbalbarrey <santiagobalbarrey@gmail.com>
2023-08-23 17:50:28 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import ItemListContainer from "./components/ItemListContainer"
+import NavBar from "./components/NavBar"
+
+
+function App() {
+
+
+  return (
+    <>
+     <NavBar/>
+     <ItemListContainer greeting={"Bienvenidos a Solartan"}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CartWidget = () => {
+  return (
+    <div>
+        🛒6
+    </div>
+  )
+}
+
+export default CartWidget
